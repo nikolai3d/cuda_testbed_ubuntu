af9c2983c26e47d641c8fc75cc32dc1180bf268d
Nikolai Svakhin <svakhine@gmail.com>
2023-12-15 21:58:25 -0800

    Added Debug/Release folders

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,29 @@
+// #include <iostream>
+
+// int main() {
+//     std::cout << "Hello, World!" << std::endl;
+//     return 0;
+// }
+
 #include <iostream>
+#include <cuda_runtime.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int deviceCount = 0;
+    cudaError_t error = cudaGetDeviceCount(&deviceCount);
+
+    if (error != cudaSuccess) {
+        std::cerr << "CUDA error: " << cudaGetErrorString(error) << std::endl;
+        return -1;
+    }
+
+    std::cout << "Number of CUDA devices: " << deviceCount << std::endl;
+
+    for (int i = 0; i < deviceCount; ++i) {
+        cudaDeviceProp deviceProp;
+        cudaGetDeviceProperties(&deviceProp, i);
+        std::cout << "Device " << i << ": " << deviceProp.name << std::endl;
+    }
+
     return 0;
 }
