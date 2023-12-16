3aa393b7dbb1ac362c0f6c918ca21c87ea933804
Nikolai Svakhin <svakhine@adobe.com>
2023-12-16 12:17:53 -0800

    Example query code

--- source/main.cpp
+++ source/main.cpp
@@ -12,6 +12,38 @@
 int main() {
     ia_nvml::query_nvml_pointers();
     
+    NVML_RT_CALL(pfn_nvmlInit_v2())
+
+    console::couti << "NVML Initialized";
+  
+    std::uint32_t device_count = 0;
+
+    NVML_RT_CALL(pfn_nvmlDeviceGetCount_v2(&device_count))
+
+    console::couti << "NVML Device count: " << device_count;
+
+    ML_EXPECTS(device_count > 0);
+
+    nvmlDevice_t device = nullptr;
+
+    console::couti << "NVML Acquiring Device PTR: ";
+
+    NVML_RT_CALL(pfn_nvmlDeviceGetHandleByIndex_v2(0, &device))
+
+    ML_EXPECTS(device!=nullptr);
+
+    std::vector<char> device_name;
+    device_name.resize(1024, '\0');
+    
+    NVML_RT_CALL(pfn_nvmlDeviceGetName(device, device_name.data(), 1023))
+
+    console::couti << "NVML device name: `" << std::string(device_name.data()) << "`";
+
+    NVML_RT_CALL(pfn_nvmlShutdown())
+
+    console::couti << "NVML Shut down";
+
+
     int deviceCount = 0;
     cudaError_t error = cudaGetDeviceCount(&deviceCount);
 
